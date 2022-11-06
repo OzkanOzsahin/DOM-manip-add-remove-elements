e6394dcdb3c48ae4fae5543e0dc7a76221998361
Ozkan Ozsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2022-11-06 11:40:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//  Deel 1 codeer binnen inspectie van je browser
+
+const listElement = document.createElement("li");
+document.body.innerHTML = ("ik ben de nieuwe inhoud");
+
+// deel 2 
+
+listItem.classList.add("list-item-class");
+console.log(paragraphElement);
+
+// deel 3
+
+const list = document.querySelector("ul");
+const paragraphElements = document.getElementsByClassName("paragraph");
+console.log(paragraphElements);
+
+
+// deel 4 
+
+const listItem = document.createElement("listItem");
+list.appendChild(listItem);
+
